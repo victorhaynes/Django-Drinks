dc035bd24f93027f7e28501d8c6b15c8cafb96bc
Victor Haynes <haynesvic@gmail.com>
2023-08-15 14:10:25 -0500

    added ability to view django generated http pages or raw json

--- drinks/views.py
+++ drinks/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 @api_view(['GET','POST'])
-def drink_list(request):
+def drink_list(request, format=None):
 
     if request.method == 'GET':
         drinks = Drink.objects.all()
@@ -23,7 +23,7 @@ def drink_list(request):
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def drink_detail(request, id):
+def drink_detail(request, id, format=None):
 
     try:
         drink = Drink.objects.get(pk=id)
