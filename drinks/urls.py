620a43f6b6d0272eb5cdb7e474914edadd9332f3
Victor Haynes <haynesvic@gmail.com>
2023-08-15 13:21:09 -0500

    finished endpoint for index action for drink model

--- drinks/urls.py
+++ drinks/urls.py
@@ -5,7 +5,8 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
+    1. Add an import:  from my_app import viewsfrom drinks import views
+
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
@@ -16,7 +17,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drinks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('drinks/', views.drink_list)
 ]
